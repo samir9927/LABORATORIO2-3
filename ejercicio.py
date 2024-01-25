0e3840568a51c98ddef26a823abc1e159d3494af
samir9927 <121317991+samir9927@users.noreply.github.com>
2024-01-25 16:42:48 -0500

    Ejercicio1.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,9 +1,17 @@
-import random
-def desordena(lista, largoLista,contador):
-    if contador<largoLista:
-        numeroRandom=random.randint(contador,largoLista-1)
-        lista[contador],lista[numeroRandom]=lista[numeroRandom],lista[contador]
-        desordena(lista,largoLista,contador+1)
-A=[2,4,6,8,10,12,14]
-desordena(A,len(A),0)
-print(A)
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+
+if numero2 != 0:
+    division = numero1 / numero2
+else:
+    division = "No se puede dividir por cero."
+
+# Mostrar resultados
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
